081d03a2ceac67fc69fe823348ebaaa6df3b2f01
NaHid Hasan Meraj <nahid.meraj765@gmail.com>
2024-09-01 23:40:36 +0600

    first commit

---
+++ numpy/vid1~num-array.py
@@ -0,0 +1,10 @@
+# python List vs numpy Array
+import numpy as np
+
+x = np.array([1,2,3,4])
+print(x) # [1 2 3 4] -- array
+print(type(x)) # <class 'numpy.ndarray'>
+
+y = [1,2,3,4]
+print(y) # [1, 2, 3, 4] -- list
+print(type(y)) # <class 'list'>
---
+++ numpy/vid3~array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+x = [1,2,3,4]
+y = np.array(x) # creating an array using numpy
+z = np.array([1,2,3,4,5])
+
+print(y) # [1 2 3 4]
+print(z) # [1 2 3 4 5]
